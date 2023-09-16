3c3e81c260a687c11ebb98556ec00d20b5b4b74a
MelonFruit7 <leogofman23@gmail.com>
2023-09-16 15:47:01 -0400

    Saved Ver, this is not finished

--- maze.c
+++ maze.c
@@ -1,16 +1,28 @@
 #include <gtk/gtk.h>
+#include <cairo.h>
+#include <stdlib.h>
+#include <time.h>
 
+int lineLength = 5; //Changing this value will more than likely break the program
+int windowWidth = 500, windowHeight = 500; //Changing these values will more than likely break the program (Should be a multiple of lineLength)
 
-
-int windowWidth = 500, windowHeight = 500;
-
+int** board; //Board for maze generation
 
 
 void defineCSS(GtkWidget *widget, GtkCssProvider *cssProvider, char* class);
+gboolean drawing(GtkWidget *widget, cairo_t *cr, gpointer data);
+void createMaze(cairo_t *cr, int x, int y);
 
 void activate(GtkApplication *app, gpointer user_data) {
+	board = malloc(windowWidth/lineLength * sizeof(int*));
+	for (int i = 0; i < windowHeight/lineLength; i++) {
+		board[i] = malloc(windowWidth/lineLength * sizeof(int));
+		for (int j = 0; j < windowWidth/lineLength; j++) board[i][j] = 0;
+	}
+
 	GtkWidget *window = gtk_application_window_new(app);
 	gtk_window_set_default_size(GTK_WINDOW(window), windowWidth, windowHeight);
+	gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
 	gtk_container_set_border_width(GTK_CONTAINER(window), 10);
 	gtk_window_set_title(GTK_WINDOW(window), "aMAZEing");
 	
@@ -23,6 +35,9 @@ void activate(GtkApplication *app, gpointer user_data) {
 	GtkWidget *startBtn = gtk_button_new_with_label("Start");
 	gtk_box_pack_start(GTK_BOX(vBox), startBtn, FALSE, FALSE, 0);
 
+	GtkWidget *darea = gtk_drawing_area_new();
+	gtk_box_pack_start(GTK_BOX(vBox), darea, TRUE, TRUE, 0);
+	g_signal_connect(G_OBJECT(darea), "draw", G_CALLBACK(drawing), NULL);
 	
 
 	defineCSS(startBtn, cssProvider, "navBtn");
@@ -41,6 +56,52 @@ int main(int argc, char** argv) {
 	return ret;
 }
 
+
+gboolean drawing(GtkWidget *widget, cairo_t *cr, gpointer data) {
+  cairo_set_source_rgb(cr, 0, 0, 0);
+  cairo_set_line_width(cr, 2);
+  srand(time(0));
+	
+  int x = windowWidth/2, y = windowHeight/2;
+  createMaze(cr, x, y);
+
+  cairo_stroke(cr); 
+  return TRUE;  
+}
+void createMaze(cairo_t *cr, int x, int y) {
+	cairo_move_to(cr, x, y);
+	int boardX = x/lineLength, boardY = y/lineLength; //The board x and y has to be adjusted.
+	board[boardY][boardX] = 1;
+	switch(rand()%4) {
+		case 0: 
+			if (boardX+1 < windowWidth/lineLength && board[boardY][boardX+1] != 1) {
+				board[boardY][boardX+1] = 1;
+				cairo_line_to(cr, x+lineLength, y);
+				createMaze(cr, x+lineLength, y);
+			}	
+		break;
+		case 1: 
+			if (boardY+1 < windowHeight/lineLength && board[boardY+1][boardX] != 1) {
+				board[boardY+1][boardX] = 1;
+				cairo_line_to(cr, x, y+lineLength);
+				createMaze(cr, x, y+lineLength);
+			}
+		break;
+		case 2: 		 
+			if (boardX-1 >= 0 && board[boardY][boardX-1] != 1) {
+				board[boardY][boardX-1] = 1;
+				cairo_line_to(cr, x-lineLength, y);
+				createMaze(cr, x-lineLength, y);
+			}
+		break;
+		case 3: 
+			if (boardY-1 >= 0 && board[boardY-1][boardX] != 1) {
+				board[boardY-1][boardX] = 1;
+				cairo_line_to(cr, x, y-lineLength);
+				createMaze(cr, x, y-lineLength);
+			}
+	}
+}
 
 void defineCSS(GtkWidget *widget, GtkCssProvider *cssProvider, char* class) { //Used to define css for a widget
   GtkStyleContext *context = gtk_widget_get_style_context(widget);
